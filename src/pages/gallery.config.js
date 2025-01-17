42a17dbf4f12dc6d46153ca59456093479c39ac6
RawJrk <rodjer7777@gmail.com>
2025-01-17 22:05:08 +0200

    Added gallery page

---
+++ src/pages/gallery.config.js
@@ -0,0 +1,9 @@
+import sharedMetadata from "../sharedMetadata.js";
+
+export const title = "R.A.W's Gallery";
+
+export const metadata = {
+  ...sharedMetadata,
+};
+
+export const navigation = null;
