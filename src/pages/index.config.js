d75cf3fdf2d5ba66bf4478434ebcb6cc1ccc7667
RawJrk <rodjer7777@gmail.com>
2025-02-02 19:28:51 +0200

    Homepage re-brending

--- src/pages/index.config.js
+++ src/pages/index.config.js
@@ -8,5 +8,5 @@ export const metadata = sharedMetadata;
 
 export const navigation = navLinks.filter((l) => l.href !== "/");
 
-export const styles = cssHref(["global"]);
+export const styles = cssHref(["global", "pages/home"]);
 export const scripts = jsSrc(["lib/gib-core", "lib/gib-events", "lib/datamosh", "pages/home"]);
