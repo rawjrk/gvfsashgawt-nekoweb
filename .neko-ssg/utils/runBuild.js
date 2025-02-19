cc7a9aba41195c389d4bb42fb08e686f63aeba72
RawJrk <rodjer7777@gmail.com>
2025-02-19 02:50:33 +0200

    Hiding compression stats on dev build

--- .neko-ssg/utils/runBuild.js
+++ .neko-ssg/utils/runBuild.js
@@ -22,39 +22,61 @@ import { formatSize, roundNumber } from "./formatSize.js";
 
 /**
  * Runs build: compile EJS to HTML, copy JS/CSS and static files.
- * @param {CopyOptions} options configuration object
+ * @param {object} options configuration object
+ * @param {boolean} options.skipMinification specifies whether to skip minification step
+ * @param {boolean} options.hideStats specifies whether to hide compression stats
  */
-export default async function runBuild({ skipMinification = false } = {}) {
+export default async function runBuild({ skipMinification = false, hideStats = false } = {}) {
   await clearBuildDir();
 
-  const [htmlOrig, htmlComp] = await buildPages({ skipMinification });
-  const [cssOrig, cssComp] = await copyStyles({ skipMinification });
-  const [jsOrig, jsComp] = await copyScripts({ skipMinification });
+  const htmlStats = await buildPages({ skipMinification });
+  const cssStats = await copyStyles({ skipMinification });
+  const jsStats = await copyScripts({ skipMinification });
 
   await copyStaticFiles();
   await copyFaviconIco();
 
-  const originalSize = htmlOrig + cssOrig + jsOrig;
-  const compressedSize = htmlComp + cssComp + jsComp;
-
-  if (!skipMinification) {
-    console.log("Code size (before):", formatSize(originalSize));
-    console.log("Code size (after):", formatSize(compressedSize));
-
-    const formatRateDiff = (orig, comp) => {
-      const rate = roundNumber(orig / comp, 2);
-      const diff = formatSize(orig - comp);
-      return `x${rate} (${diff})`;
-    };
-
-    console.log("Compression:", formatRateDiff(originalSize, compressedSize));
-
-    console.log(" * HTML\t:", formatRateDiff(htmlOrig, htmlComp));
-    console.log(" * CSS\t:", formatRateDiff(cssOrig, cssComp));
-    console.log(" * JS\t:", formatRateDiff(jsOrig, jsComp));
-  } else {
-    console.log("Code size (uncompressed):", formatSize(originalSize));
+  if (!hideStats) {
+    logStats({ htmlStats, cssStats, jsStats }, skipMinification);
   }
 
   console.log("Successfully completed fresh build");
+}
+
+/**
+ * Logs statistics about code files compression.
+ * @param {object} stats statistic object
+ * @param {CopyResult} stats.htmlStats statistics on HTML files
+ * @param {CopyResult} stats.cssStats statistics on CSS files
+ * @param {CopyResult} stats.jsStats statistics on JS files
+ * @param {boolean} skipMinification specifies whether to skip minification step
+ * @returns {void}
+ */
+function logStats({ htmlStats, cssStats, jsStats }, skipMinification = false) {
+  const [htmlOrig, htmlComp] = htmlStats;
+  const [cssOrig, cssComp] = cssStats;
+  const [jsOrig, jsComp] = jsStats;
+
+  const originalSize = htmlOrig + cssOrig + jsOrig;
+  const compressedSize = htmlComp + cssComp + jsComp;
+
+  if (skipMinification) {
+    console.log("Code size (uncompressed):", formatSize(originalSize));
+    return;
+  }
+
+  console.log("Code size (before):", formatSize(originalSize));
+  console.log("Code size (after):", formatSize(compressedSize));
+
+  const formatRateDiff = (orig, comp) => {
+    const rate = roundNumber(orig / comp, 2);
+    const diff = formatSize(orig - comp);
+    return `x${rate} (${diff})`;
+  };
+
+  console.log("Compression:", formatRateDiff(originalSize, compressedSize));
+
+  console.log(" * HTML\t:", formatRateDiff(htmlOrig, htmlComp));
+  console.log(" * CSS\t:", formatRateDiff(cssOrig, cssComp));
+  console.log(" * JS\t:", formatRateDiff(jsOrig, jsComp));
 }
