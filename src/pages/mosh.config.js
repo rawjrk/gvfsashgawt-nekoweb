d71e0204c2781e886fe6d54e84fc3cc701cd2a0b
RawJrk <rodjer7777@gmail.com>
2025-02-26 19:56:31 +0200

    Updated mosh metadata

--- src/pages/mosh.config.js
+++ src/pages/mosh.config.js
@@ -1,12 +1,14 @@
 import sharedMetadata, { HOST_URL } from "../sharedMetadata.js";
 import navLinks from "../navLinks.js";
 
-export const title = "Destroy Data, Not People";
+export const title = "Destroy Data, Not People!";
+
+const RED_SQUARE_EMOJI = "\uD83D\uDFE5";
 
 export const metadata = {
   ...sharedMetadata,
-  "og:title": title,
-  "og:description": "Web-tooling for animated Jpeg datamosh",
+  "og:title": `${RED_SQUARE_EMOJI} ${title}`,
+  "og:description": "Webtool for LIVE GLITCH animations",
   "og:image": `${HOST_URL}/static/thumb/mosh.png`,
   "og:url": `${HOST_URL}/mosh`,
 };
