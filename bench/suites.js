3511ed2e363378fb849e2ac3a1ac37c3b3f215ef
RawJrk <rodjer7777@gmail.com>
2025-08-17 19:49:31 +0300

    Add page with bench suites (UI)

--- bench/suites.js
+++ bench/suites.js
@@ -12,20 +12,45 @@ const suite = new Benchmark.Suite({
   },
 });
 
+const testDiv = document.getElementById("test-div");
+const testCanvas = document.getElementById("test-canvas");
+
 const moshRange_bytes = [345, 375];
+const mimeType = "image/jpeg";
 
 const datamoshed1 = new DatamoshedImage();
 datamoshed1.setDatamoshRange(...moshRange_bytes);
+const renderDatamosh1 = () => {
+  const content = datamoshed1.generateMoshedBase64();
+  const imageData = `url("data:${mimeType};base64,${content}")`;
+  testDiv.style.background = imageData;
+};
+
+const datamoshed2 = new DatamoshedImage();
+datamoshed2.setDatamoshRange(...moshRange_bytes);
+const renderDatamosh2 = async () => {
+  const content = datamoshed2.generateMoshedBase64();
+  await loadBase64ToCanvas(testCanvas, content, mimeType);
+};
 
 // Setup test cases for benchmarking
-suite.add("DatamoshedImage (current)", function () {
-  datamoshed1.generateMoshedBase64();
-});
+suite
+  .add("DatamoshedImage (background)", function () {
+    renderDatamosh1();
+  })
+  .add("DatamoshedImage (canvas)", {
+    defer: true,
+    fn: async function (deferred) {
+      await renderDatamosh2();
+      deferred.resolve();
+    },
+  });
 
 (async () => {
   // load images
   const imageUrl = `static/images/apple-tree-color-256px.jpeg`;
   await datamoshed1.fetchImage(imageUrl);
+  await datamoshed2.fetchImage(imageUrl);
 
   suite.run({ async: true });
 })();
