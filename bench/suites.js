43bd74f704efd53d4e5341d5a7e6c4bd55e125a6
RawJrk <rodjer7777@gmail.com>
2025-08-17 19:49:31 +0300

    Add page with bench suites (UI)

--- bench/suites.js
+++ bench/suites.js
@@ -12,21 +12,34 @@ const suite = new Benchmark.Suite({
   },
 });
 
+const testDiv = document.getElementById("test-div");
+const testCanvas = document.getElementById("test-canvas");
+
 const moshRange_bytes = [345, 375];
+const mimeType = "image/jpeg";
 
 const datamoshed1 = new DatamoshedImage();
 datamoshed1.setDatamoshRange(...moshRange_bytes);
+const renderDatamosh1 = () => {
+  const content = datamoshed1.generateMoshedBase64();
+  const imageData = `url("data:${mimeType};base64,${content}")`;
+  testDiv.style.background = imageData;
+};
 
 const datamoshed2 = new DatamoshedFile();
 datamoshed2.setDatamoshRange(...moshRange_bytes);
+const renderDatamosh2 = async () => {
+  const bytes = datamoshed2.generateMoshedBytes();
+  await loadBytesToCanvas(testCanvas, bytes, mimeType);
+};
 
 // Setup test cases for benchmarking
 suite
   .add("DatamoshedImage (current)", function () {
-    datamoshed1.generateMoshedBase64();
+    renderDatamosh1();
   })
   .add("DatamoshedFile (new)", function () {
-    datamoshed2.generateMoshedBytes();
+    return renderDatamosh2();
   });
 
 (async () => {
